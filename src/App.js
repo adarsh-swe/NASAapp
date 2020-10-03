308a23df09923d3aa73fb119b4099d5ff762414b
Adarsh Patel <swe.adarsh@gmail.com>
2020-10-03 14:00:48 -0400

    react routing added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from "react";
 import GetInfo from "./Components/GetInfo";
+import Login from "./Components/Login";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch,Link } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Container className="mt-5">
-        <GetInfo />
-      </Container>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Getinfo</Link>
+            </li>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            {GetInfo}
+          </Route>
+          <Route path="/login" exact>
+            {Login}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
