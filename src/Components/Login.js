1e7efcb0e6781551a1b0af765a56a2775b503f55
Adarsh Patel <swe.adarsh@gmail.com>
2020-10-03 15:30:56 -0400

    new file register

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import { Button, FormGroup, FormControl, FormLabel} from "react-bootstrap";
-import {useState} from 'react';
+import React, { useState } from 'react';
+import { Button, Form} from "react-bootstrap";
 import { Link } from 'react-router-dom';
 
 
 export default function Login() {
-    //const [email, setEmail] = useState("");
-    //const [password, setPassword] = useState("");
-  
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     
+    const handleChange = () =>{
+
+    }
+    
+    /*
+        <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+    */
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        
-        <div>
-            <h1>TEMP</h1>
-        </div>
+        <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control 
+                    type="email" 
+                    placeholder="Enter email" 
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control 
+                    type="password" 
+                    placeholder="Password" 
+                    onChange={e => setPassword(e.target.value)}
+                />
+            </Form.Group>
+            <Button 
+                variant="primary" 
+                type="submit"
+                disabled={!validateForm()}
+            >
+                Submit
+            </Button>
+        </Form>
     );
   }
