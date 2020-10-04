f2ab1029879ebc7af249633e99170838e952bac8
Madhu Sivapragasam <madhuran.siva@gmail.com>
2020-10-03 21:45:06 -0400

    Changes

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -5,12 +5,33 @@ import {Container, Col, Row } from "react-bootstrap";
 function Grid(/*{time}, {day1}, {day2}, {day3}*/) {
     return (
         <Container>
-            <Row>
-                <Col className = "col">Time{/*{time}*/}</Col>
-                <Col className = "col"> Day1{/*{Day1}*/}</Col>
-                <Col className = "col">Day2{/*{Day2}*/}</Col>
-                <Col className = "col">Day3{/*{Day3}*/}</Col>
-            </Row>
+            <Col >
+                <Row className = "col">Header{/*{header}*/}</Row>
+                <Row className = "col"> 0{/*0*/}</Row>
+                <Row className = "col"> 1{/*{1}*/}</Row>
+                <Row className = "col">2{/*{2}*/}</Row>
+                <Row className = "col">3{/*{3}*/}</Row>
+                <Row className = "col">4{/*{4}*/}</Row>
+                <Row className = "col">5{/*{5}*/}</Row>
+                <Row className = "col">6{/*{6}*/}</Row>
+                <Row className = "col">7{/*{7}*/}</Row>
+                <Row className = "col">8{/*{8}*/}</Row>
+                <Row className = "col">9{/*{9}*/}</Row>
+                <Row className = "col">10{/*{10}*/}</Row>
+                <Row className = "col">11{/*{11}*/}</Row>
+                <Row className = "col">12{/*{12}*/}</Row>
+                <Row className = "col">13{/*{13}*/}</Row>
+                <Row className = "col">14{/*{14}*/}</Row>
+                <Row className = "col">15{/*{15}*/}</Row>
+                <Row className = "col">16{/*{16}*/}</Row>
+                <Row className = "col">17{/*{17}*/}</Row>
+                <Row className = "col">18{/*{18}*/}</Row>
+                <Row className = "col">19{/*{19}*/}</Row>
+                <Row className = "col">20{/*{20}*/}</Row>
+                <Row className = "col">21{/*{21}*/}</Row>
+                <Row className = "col">22{/*{22}*/}</Row>
+                <Row className = "col">23{/*{23}*/}</Row>
+            </Col>
         </Container>
     );
 }
