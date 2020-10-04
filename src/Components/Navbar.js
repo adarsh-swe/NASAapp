5d5b9af8cf360f63dca5ff61f3b334d0c364b63c
Adarsh Patel <swe.adarsh@gmail.com>
2020-10-03 21:57:18 -0400

    updating

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,38 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import firebase from "firebase";
+import {auth} from "../config"
 
 function Navbar() {
-    return (
-        <div className="topnav">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Getinfo</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/grid">Output</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-        </nav>
-        </div>
-    );
+  const user = auth.currentUser;
+
+  return (user ? 
+    <div className="topnav">
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Getinfo</Link>
+        </li>
+        <li>
+          <Link to="/grid">Output</Link>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  : 
+    <div className="topnav">
+    <nav>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  );
 
 }
 
